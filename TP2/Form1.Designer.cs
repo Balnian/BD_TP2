4d360416f69907497547e9084caad688611341f9
Francis <super_f_qc@hotmail.com>
2015-09-25 16:30:39 -0400

    resize label recherche form1

--- TP2/Form1.Designer.cs
+++ TP2/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.DGV_Inventaire = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rafraichirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.TB_Recherche = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.ajouterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,12 +40,10 @@
             this.listerInventaireBasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.rafraichirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Inventaire)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DGV_Inventaire
@@ -62,12 +62,27 @@
             this.DGV_Inventaire.Size = new System.Drawing.Size(523, 281);
             this.DGV_Inventaire.TabIndex = 0;
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rafraichirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(166, 26);
+            // 
+            // rafraichirToolStripMenuItem
+            // 
+            this.rafraichirToolStripMenuItem.Name = "rafraichirToolStripMenuItem";
+            this.rafraichirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.rafraichirToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.rafraichirToolStripMenuItem.Text = "Rafraichir";
+            this.rafraichirToolStripMenuItem.Click += new System.EventHandler(this.rafraichirToolStripMenuItem_Click);
+            // 
             // TB_Recherche
             // 
             this.TB_Recherche.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TB_Recherche.Location = new System.Drawing.Point(63, 0);
+            this.TB_Recherche.Location = new System.Drawing.Point(77, 0);
             this.TB_Recherche.Name = "TB_Recherche";
-            this.TB_Recherche.Size = new System.Drawing.Size(460, 20);
+            this.TB_Recherche.Size = new System.Drawing.Size(446, 20);
             this.TB_Recherche.TabIndex = 2;
             this.TB_Recherche.TextChanged += new System.EventHandler(this.TB_Recherche_TextChanged);
             // 
@@ -121,9 +136,10 @@
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(77, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Recherche:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -138,21 +154,6 @@
             this.panel1.Size = new System.Drawing.Size(523, 21);
             this.panel1.TabIndex = 5;
             // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.rafraichirToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(166, 26);
-            // 
-            // rafraichirToolStripMenuItem
-            // 
-            this.rafraichirToolStripMenuItem.Name = "rafraichirToolStripMenuItem";
-            this.rafraichirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
-            this.rafraichirToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.rafraichirToolStripMenuItem.Text = "Rafraichir";
-            this.rafraichirToolStripMenuItem.Click += new System.EventHandler(this.rafraichirToolStripMenuItem_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -165,11 +166,11 @@
             this.Text = "Inventaire";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Inventaire)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ TP2/Modifier.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2
+{
+    public partial class Modifier : Form
+    {
+        private int IdInventaire;
+        private DataSet Data;
+        private DataSet DataFournisseur;
+
+        private int CurrStock;
+        private int CurrMax;
+        private int CurrMin;
+        private int CurrFournisseur;
+        private String CurrDescription;
+        public Modifier(int IdInventaire)
+        {
+            InitializeComponent();
+            this.IdInventaire = IdInventaire;
+        }
+
+        private void getData()
+        {
+            Dictionary<String, Object> dico = new Dictionary<String, Object>();
+
+            try
+            {
+                
+                dico.Add("@IdInventaire", IdInventaire);
+                Data = DAL_Inventaire.Query("select * from Inventaire where IdInventaire = @IdInventaire", dico);
+               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            fillFournisseur();
+            registerCurrentValue();
+            ResetValues();
+        }
+
+        private void ResetValues()
+        {
+            
+            
+            NUD_Stock.Value = CurrStock;
+            NUD_MaxQte.Value = CurrMax;
+            NUD_MinQte.Value = CurrMin;
+            TB_Description.Text = CurrDescription;
+            CB_Fournisseurs.SelectedValue = CurrFournisseur;
+
+        }
+        private void registerCurrentValue()
+        {
+            DataRow Row = Data.Tables["Inventaire"].Rows[0];
+            CurrDescription = (string)Row.ItemArray[1];
+            CurrFournisseur = (int)Row.ItemArray[2];
+            CurrStock = (int)Row.ItemArray[3];
+            CurrMin = (int)Row.ItemArray[4];
+            CurrMax = (int)Row.ItemArray[5];
+        }
+
+        private void fillFournisseur()
+        {
+            Dictionary<String, Object> dico = new Dictionary<String, Object>();
+
+            try
+            {
+
+                DataFournisseur = DAL_Inventaire.Query("select IdFournisseur, NomFournisseur from Fournisseur");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            CB_Fournisseurs.DataSource = DataFournisseur.Tables["Inventaire"];
+            CB_Fournisseurs.ValueMember = "IdFournisseur";
+            CB_Fournisseurs.DisplayMember = "NomFournisseur";
+        }
+
+        
+
+        private void Modifier_Load(object sender, EventArgs e)
+        {
+
+            getData();
+
+        }
+
+        void save()
+        {
+            Dictionary<String, Object> dico = new Dictionary<String, Object>();
+
+            dico.Add("@Idinventaire", IdInventaire);
+            dico.Add("@Stock", (int)NUD_Stock.Value);
+            dico.Add("@Max", (int)NUD_MaxQte.Value);
+            dico.Add("@Min", (int)NUD_MinQte.Value);
+            dico.Add("@IdFournisseur", CB_Fournisseurs.SelectedValue);
+            dico.Add("@Description", TB_Description.Text);
+
+            try
+            {
+
+                DAL_Inventaire.NonQuery("update Inventaire set DescriptionInventaire = @Description, IdFournisseur = @IdFournisseur, QteStock = @Stock, QteMinimum = @Min, QteMaximum = @Max where IdInventaire = @Idinventaire", dico);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void enableAppliquer()
+        {
+            BT_Appliquer.Enabled = (int)NUD_Stock.Value != CurrStock || (int)NUD_MinQte.Value != CurrMin ||(int)NUD_MaxQte.Value != CurrMax || (int)CB_Fournisseurs.SelectedValue != CurrFournisseur || TB_Description.Text != CurrDescription;
+        }
+
+        private void BT_Appliquer_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        private void NUD_Stock_ValueChanged(object sender, EventArgs e)
+        {
+            enableAppliquer();
+        }
+
+        private void NUD_MaxQte_ValueChanged(object sender, EventArgs e)
+        {
+            enableAppliquer();
+        }
+
+        private void NUD_MinQte_ValueChanged(object sender, EventArgs e)
+        {
+            enableAppliquer();
+        }
+
+        private void CB_Fournisseurs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            enableAppliquer();
+        }
+
+        private void TB_Description_TextChanged(object sender, EventArgs e)
+        {
+            enableAppliquer();
+        }
+    }
+}
